c7667fa8fa9d8f19887db3df7ea723dd06cc29ac
shivani020 <109856283+shivani020@users.noreply.github.com>
2024-09-26 07:19:08 +0530

    Add files via upload

---
+++ conn.java
@@ -0,0 +1,20 @@
+package EMS;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem", "root", "Mehakyadav@2003");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
